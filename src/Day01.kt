b6d11874677d81ee9f767cb631f438c0a5d50593
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2023-12-01 12:01:41 -0500

    Solved Day 1 part 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,21 @@
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.sumOf { s ->
+            ( (s.firstOrNull { it.isDigit() } ?: '0').digitToInt() * 10
+            + (s.lastOrNull { it.isDigit() } ?: '0').digitToInt() )}
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return 281
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput1 = readInput("Day01_part1_test")
+    check(part1(testInput1) == 142)
+
+    val testInput2 = readInput("Day01_part2_test")
+    check(part2(testInput2) == 281)
 
     val input = readInput("Day01")
     part1(input).println()
