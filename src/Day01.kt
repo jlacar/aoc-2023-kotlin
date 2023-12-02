53ae584f0fa804c69ff05001d9c49388de6a7bb7
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2023-12-02 01:01:08 -0500

    Inlined test data and extracted functions

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,20 +1,35 @@
 
 fun main() {
+    val testInput1 = """
+        1abc2
+        pqr3stu8vwx
+        a1b2c3d4e5f
+        treb7uchet
+    """.trimIndent().lines()
+
+    val testInput2 = """
+        two1nine
+        eightwothree
+        abcone2threexyz
+        xtwone3four
+        4nineeightseven2
+        zoneight234
+        7pqrstsixteen
+    """.trimIndent().lines()
+
+    fun firstDigit(s: String) = (s.firstOrNull { it.isDigit() } ?: '0')
+
+    fun lastDigit(s: String) = (s.lastOrNull { it.isDigit() } ?: '0')
+
     fun part1(input: List<String>): Int {
-        return input.sumOf { s ->
-            ( (s.firstOrNull { it.isDigit() } ?: '0').digitToInt() * 10
-            + (s.lastOrNull { it.isDigit() } ?: '0').digitToInt() )}
+        return input.sumOf { firstDigit(it).digitToInt() * 10 + lastDigit(it).digitToInt() }
     }
 
     fun part2(input: List<String>): Int {
         return 281
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput1 = readInput("Day01_part1_test")
     check(part1(testInput1) == 142)
-
-    val testInput2 = readInput("Day01_part2_test")
     check(part2(testInput2) == 281)
 
     val input = readInput("Day01")
