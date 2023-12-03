607166abe6c83998225107237d4ecd46374a709f
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2023-12-03 10:21:50 -0500

    Outlined solution for Day 3

---
+++ src/Day03.kt
@@ -0,0 +1,44 @@
+fun main() {
+
+    // PART 1
+
+    fun part1(input: List<String>): Int {
+        return input.size
+    }
+
+    // PART 2
+
+    fun part2(input: List<String>): Int {
+        return input.size
+    }
+
+    // Util
+
+    fun bordered(raw: List<String>): List<String> {
+        val border = ".".repeat(raw[0].length)
+        return listOf(border) + raw + border
+    }
+
+    // Tests
+
+    val testInput = """
+        467..114..
+        ...*......
+        ..35..633.
+        ......#...
+        617*......
+        .....+.58.
+        ..592.....
+        ......755.
+        ...${'$'}.*....
+        .664.598..
+    """.trimIndent().lines()
+
+    testInput.println()
+    check(part1(bordered(testInput)) == 4361)
+
+    // Day 3 Solution
+//    val input = bordered(readInput("Day03"))
+//    println(part1(input))
+//    println(part2(input))
+}
