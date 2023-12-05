696a24307b0b60205861a05f79399a47a7911660
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2023-12-05 02:23:47 -0500

    Refactored to pattern of Todd Ginsberg's [very nice] solution

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,42 +1,45 @@
 // Day 2 solutions
+
+// Patterned after Todd Ginsberg's solution (tginsberg)
+// Aspiring to think and code in Kotlin like Todd :)
+data class Game(val id: Int, val red: Int, val green: Int, val blue: Int) {
+
+    fun isPossibleWith(red: Int, green: Int, blue: Int) =
+        this.red <= red && this.green <= green && this.blue <= blue
+
+    fun power() = red * green * blue
+
+    companion object {
+        fun of(input: String): Game {
+            val (label, sets) = input.split(": ")
+            val atLeast = mutableMapOf<String, Int>()
+            sets.split("; ").forEach { draw ->
+                draw.split(", ").forEach { cubes ->
+                    val (n, color) = cubes.split(" ")
+                    atLeast[color] = n.toInt().let { maxOf(it, atLeast[color] ?: it)
+                }
+            }
+
+            val (_, id) = label.split(" ")
+            return Game(
+                id.toInt(),
+                red = atLeast["red"] ?: 0,
+                green = atLeast["green"] ?: 0,
+                blue = atLeast["blue"] ?: 0
+            )
+        }
+    }
+}
+
 fun main() {
 
     // PART 1
-
-    val configuration = mapOf("red" to 12, "green" to 13, "blue" to 14)
-
-    fun tooMany(cubes: String) = cubes.split(", ")
-        .sumOf { it.split(" ").let { (n, color) ->
-            val drawn = n.toInt()
-            if (configuration[color]!! < drawn) drawn else 0
-        }} > 0
-
-    fun enoughCubes(draws: List<String>) = (draws.firstOrNull { cubes -> tooMany(cubes) } ?: "").isBlank()
-
-    fun possible(game: String) : Int =
-        game.split(": ").let { (id, draws) ->
-            if (enoughCubes(draws.split("; "))) { id.split(" ")[1].toInt() } else 0
-        }
-
-    fun part1(input: List<String>) = input.sumOf { game -> possible(game) }
+    fun part1(games: List<Game>) = games.filter {
+        it.isPossibleWith(red = 12, green = 13, blue = 14)
+    }.sumOf { it.id }
 
     // PART 2
-
-    fun power(drawSets: String) : Int {
-        val mostCubes = mutableMapOf("red" to 0, "blue" to 0, "green" to 0)
-        drawSets.split("; ").map { draw ->
-            draw.split(", ").map { cubes ->
-                cubes.split(" ").let { (n, color) -> color to n.toInt() }
-            }
-        }.flatten().forEach { (color, count) ->
-            if (mostCubes[color]!! < count) mostCubes[color] = count
-        }
-        return mostCubes.toList().fold(1) { acc, (_, count) -> acc * count }
-    }
-
-    fun part2(input: List<String>) : Int {
-        return input.sumOf { game -> power(game.split(": ")[1]) }
-    }
+    fun part2(games: List<Game>) = games.sumOf { it.power() }
 
     // Test Parts 1 & 2
     val testInput = """
@@ -45,13 +48,13 @@ fun main() {
         Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
         Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
         Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
-    """.trimIndent().lines()
+    """.trimIndent().lines().map { Game.of(it) }
 
     check(part1(testInput) == 8)
     check(part2(testInput) == 2286)
 
     // Day 2 solutions
-    val input = readInput("Day02")
-    check(part1(input) == 2600)
-    check(part2(input) == 86036)
+    val games = readInput("Day02").map { Game.of(it) }
+    check(part1(games) == 2600)
+    check(part2(games) == 86036)
 }
