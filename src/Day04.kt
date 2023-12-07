9773dc16fbb98181893572927a0a5b319ae52250
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2023-12-07 10:49:49 -0500

    Solved: Day 4 Part 1

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,33 +1,38 @@
+import kotlin.math.pow
+
 // --- Day 4: Scratchcards ---
 
 // Having a data class can help organize code and logic
-data class ScratchCard(val winning: List<Int>, val cardNumbers: List<Int>) {
-
-  companion object {
-      fun of(line: String): ScratchCard {
-//          val winning = listOf<Int>()
-//          val cardNumbers = listOf<Int>()
-//          return ScratchCard(winning, cardNumbers)
-          return ScratchCard(emptyList(), emptyList())
-      }
-  }
+data class ScratchCard(val winning: Set<Int>, val cardNumbers: Set<Int>) {
+    fun points(): Int {
+        val n = (winning intersect cardNumbers).size
+        return if (n == 0) 0 else 2.0.pow(n - 1).toInt()
+    }
+    companion object {
+        fun of(line: String): ScratchCard {
+            val (_, ws, cns) = line.split(": ", " | ")
+            val winning = ws.asSetOfInt(" ")
+            val cardNumbers = cns.asSetOfInt(" ")
+            return ScratchCard(winning, cardNumbers)
+        }
+    }
 }
+
+fun String.asSetOfInt(vararg delimiters: String): Set<Int> =
+    this.split(*delimiters).filter { it.isNotBlank() }.map { it.toInt() }.toSet()
 
 fun main() {
 
     // DSL Extensions
     fun List<String>.asCards(): List<ScratchCard> {
-//        map { line ->
-//            val (ws, cns) =
-//        }
-        return emptyList()
+        return map { ScratchCard.of(it) }
     }
 
     // PART 1
 
     fun part1(input: List<String>): Int {
         val cards = input.asCards()
-        return input.size
+        return cards.sumOf { it.points() }
     }
 
     // PART 2
@@ -41,7 +46,7 @@ fun main() {
     val testSmallInput1 = """
         Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
     """.trimIndent().lines()
-    check(part1(testSmallInput1) == 1)
+    check(part1(testSmallInput1) == 8)
     check(part2(testSmallInput1) == 1)
 
     val testSmallInput2 = """
@@ -49,6 +54,7 @@ fun main() {
         Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
         Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
     """.trimIndent().lines()
+
     check(part1(testSmallInput2) == 2 + 1 + 0)
     check(part2(testSmallInput2) == 3)
 
@@ -61,8 +67,8 @@ fun main() {
             Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
         """.trimIndent().lines()
 
-    check(part1(testSampleFromAoC1).also { it.println() }
-        == 13)
+    check(part1(testSampleFromAoC1)
+            == 13)
 
     val testSampleFromAoC2 = """
         paste.aoc.sample.here
@@ -70,10 +76,10 @@ fun main() {
     check(part2(testSampleFromAoC2) == 1)
 
     // Day X Solution
-    val input = readInput("DayXX")
+    val input = readInput("Day04")
 
-//    check(part1(input) == ???).also { it.println() }
-    "Part 1 --> ${part1(input)}".println()
+    check(part1(input) == 22_193)
+//    "Part 1 --> ${part1(input)}".println()
 
 //    check(part2(input) == ???).also { it.println() }
     "Part 2 --> ${part2(input)}".println()
