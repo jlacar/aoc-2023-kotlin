25bb8aabb65c4605540105f413d455c8458e3363
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2023-12-16 05:10:04 -0500

    Replicated Todd Ginnsburg's solution for Day 5 Part 2 by using a sequence and reverse lookup. Part 2 finish ~6-7 secs vs. ~6-7 mins

--- src/Day05tg.kt
+++ src/Day05tg.kt
@@ -1,10 +1,28 @@
 /* Day 5 - Attempt to think like Todd Ginsburg, now that I've seen his solution */
 
-class Day05tg(val seeds: List<Long>, val conversionTables: List<ConversionTable>) {
+class Day05tg(val seeds: List<Long>, private val conversionTables: List<ConversionTable>) {
 
-    fun part1(): Long = 0L
+    val seedRanges: List<LongRange> = seeds.chunked(2) { (first, length) ->
+        first..<(first + length)
+    }
 
-    fun part2(): Long = 0L
+    fun part1(): Long = seeds.minOf { convert(it) }
+
+    fun part2(): Long {
+        val reverseLookup = reversedConversionTable()
+        return generateSequence(0, Long::inc).first { location ->
+            val seed = convert(location, reverseLookup)
+            seedRanges.any { it.contains(seed) }
+        }
+    }
+
+    private fun reversedConversionTable(): List<ConversionTable> = conversionTables.reversed()
+        .map { it.reversed() }
+
+    private fun convert(seed: Long, conversions: List<ConversionTable> = conversionTables): Long =
+        conversions.fold(seed) { source: Long, lookupTable: ConversionTable ->
+            lookupTable.convert(source)
+        }
 
     companion object {
         fun using(input: List<String>) = Day05tg (
@@ -12,18 +30,45 @@ class Day05tg(val seeds: List<Long>, val conversionTables: List<ConversionTable>
             parseConversionTables(input)
         )
 
-        private fun parseConversionTables(input: List<String>): List<ConversionTable> {
-            TODO("Not yet implemented")
-        }
+        private fun parseSeeds(input: List<String>): List<Long> =
+            input.first().substringAfter(": ").asListOfLong(" ")
 
-        private fun parseSeeds(input: List<String>): List<Long> {
-            TODO("Not yet implemented")
-        }
+        private fun parseConversionTables(input: List<String>): List<ConversionTable> =
+            input.drop(2)
+                .joinToString("\n").split("\n\n")
+                .map { table -> ConversionTable.parse(table.lines()) }
     }
 }
 
-class ConversionTable {
+data class ConversionTable(val description: String, val conversions: List<Conversion>) {
+    fun convert(value: Long): Long = conversions.firstOrNull { it.contains(value) } ?.convert(value) ?: value
+    fun reversed() = ConversionTable("reversed", conversions.map { it.reversed() })
 
+    companion object {
+        fun parse(conversions: List<String>) = ConversionTable (
+            conversions.first(),
+            conversions.drop(1).map { Conversion.parse(it) }
+        )
+    }
+}
+
+data class Conversion(val destination: LongRange, val source: LongRange) {
+    fun convert(sourceValue: Long): Long =
+        if (sourceValue in source) destination.first + sourceValue - source.first else sourceValue
+
+    fun contains(value: Long) = source.contains(value)
+
+    fun reversed() = Conversion(source, destination)
+
+    companion object {
+        fun parse(input: String): Conversion {
+            val (destStart, sourceStart, length) = input.asListOfLong(" ")
+            return Conversion(
+                destStart..<(destStart + length),
+                sourceStart..<(sourceStart + length)
+            )
+        }
+    }
 }
 
 fun main() {
@@ -68,7 +113,7 @@ fun main() {
         val actual1 = part1()
         val expected1: Long = 35
         check( expected1 == actual1 ) {
-            lazyMessage("Part 1 sample data", expected1, actual1)
+            lazyMessage("Part 1 sample data", expected1, actual1, "seeds: $seeds")
         }
 
         val actual2 = part2()
@@ -78,7 +123,7 @@ fun main() {
         }
     }
 
-    check(false) {
+    check(true) {
         """
         |
         | All tests PASS! To see problem solution:
@@ -90,7 +135,7 @@ fun main() {
     "SOLUTION".println()
 
     // Part 1
-    Day05.using(readInput("Day05")).apply {
+    Day05tg.using(readInput("Day05")).apply {
         val actual = part1().also { "Part 1 -> $it".println() }
         val correctAnswer: Long = 600279879
 
@@ -100,7 +145,7 @@ fun main() {
     }
 
     // Part 2
-    Day05.using(readInput("Day05")).apply {
+    Day05tg.using(readInput("Day05")).apply {
         val actual = part2().also { "Part 2 -> $it".println() }
         val correctAnswer: Long = 20191102
 
