ac7ce2c4b37958534d3099d21799ed24a6d5c22d
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2023-12-18 18:53:49 -0500

    Solved Day8 Part 2 - ugly. Had to get tip for the approach

--- src/Day08.kt
+++ src/Day08.kt
@@ -2,22 +2,53 @@
  * --- Day 8: Haunted Wasteland ---
  */
 
-class Day08(val instructions: String, val nodes: Map<String, Pair<String, String>>) {
-    fun part1(): Int {
-        val wrapLength = instructions.length
-        var lastVisited = "AAA"
-        val steps = generateSequence(0, Int::inc).indexOfFirst { i: Int ->
-            val nextNode = nodes[lastVisited] ?: error("Node [$lastVisited] not found!")
-            val next = nextNode.pick(instructions[i % wrapLength])
-            lastVisited = next
-            next == "ZZZ"
+typealias Node = Pair<String, String>
+
+class Day08(val instructions: String, val nodes: Map<String, Node>) {
+
+    fun part1(): Long = stepCountFor("AAA") { label -> label == "ZZZ" }.toLong()
+
+    fun part2(): Long {
+        val steps = nodesEndingWithA().map {
+            stepCountFor(it.key) { label -> label.endsWith('Z') }
         }
-        return steps + 1
+        return leastCommonMultiple(steps)
     }
 
-    private fun Pair<String, String>.pick(side: Char) = if (side == 'L') first else second
+    private fun nodesEndingWithA() = nodes.filter { it.key.endsWith('A') }
 
-    fun part2(): Int = 0
+    private fun leastCommonMultiple(steps: List<Int>): Long {
+        return steps.fold(1L) { prod: Long, n: Int -> lcm(prod, n.toLong()) }
+    }
+
+    private fun lcm(num1: Long, num2: Long): Long {
+        val larger = if (num1 > num2) num1 else num2
+        val maxLcm = num1 * num2
+        var lcm = larger
+        while (lcm <= maxLcm) {
+            if (lcm % num1 == 0L && lcm % num2 == 0L) {
+                return lcm
+            }
+            lcm += larger
+        }
+        return maxLcm
+    }
+
+    private fun Node.pick(side: Char) = if (side == 'L') first else second
+
+    private val wrapLength = instructions.length
+    private fun side(index: Int) = instructions[index % wrapLength]
+
+    private fun stepCountFor(start: String, isEndLabel: (String) -> Boolean): Int {
+        val lastVisited =  mutableListOf(start)
+        return generateSequence(0, Int::inc).indexOfFirst { index: Int ->
+            lastVisited.replaceAll { label ->
+                val nextNode = nodes[label] ?: error("Node[$label] does not exist!")
+                nextNode.pick(side(index))
+            }
+            lastVisited.removeAll(isEndLabel)
+        }.inc()
+    }
 
     companion object {
         fun using(input: List<String>) = Day08(
@@ -51,26 +82,15 @@ fun main() {
         """.trimIndent().lines()
 
     Day08.using(sampleInput1).apply {
-
         with (part1()) {
             "Part 1 (example 1) -> $this".println()
 
-            val expected = 2
+            val expected: Long = 2
             check(this == expected) {
                 lazyMessage("Part 1 (example 1)", expected, this)
             }
         }
-
-//        with (part2()) {
-//            "Part 2 (sample) -> $this".println()
-//
-//            val expected = ?
-//            check(this == expected) {
-//                lazyMessage("Part 2 (example)", expected, this)
-//            }
-//        }
     }
-
 
     val sampleInput2 =
         """
@@ -82,27 +102,41 @@ fun main() {
         """.trimIndent().lines()
 
     Day08.using(sampleInput2).apply {
-
         with (part1()) {
             "Part 1 (example 2) -> $this".println()
 
-            val expected = 6
+            val expected: Long = 6
             check(this == expected) {
                 lazyMessage("Part 1 (example 2)", expected, this)
             }
         }
-
-//        with (part2()) {
-//            "Part 2 (sample) -> $this".println()
-//
-//            val expected = ?
-//            check(this == expected) {
-//                lazyMessage("Part 2 (example)", expected, this)
-//            }
-//        }
     }
 
-//    check(false) {"Temp breakpoint"}
+    val sampleInputPart2 =
+        """
+        LR
+        
+        11A = (11B, XXX)
+        11B = (XXX, 11Z)
+        11Z = (11B, XXX)
+        22A = (22B, XXX)
+        22B = (22C, 22C)
+        22C = (22Z, 22Z)
+        22Z = (22B, 22B)
+        XXX = (XXX, XXX)        
+        """.trimIndent().lines()
+
+    Day08.using(sampleInputPart2).apply {
+        with (part2()) {
+            "Part 2 (sample) -> $this".println()
+
+            val expected: Long = 6
+            check(this == expected) {
+                lazyMessage("Part 2 (example)", expected, this)
+            }
+        }
+
+    }
 
     check(true) {
         """
@@ -113,6 +147,7 @@ fun main() {
         """.trimMargin()
     }
 
+
     "SOLUTION".println()
 
     val myPuzzleInput = readInput("Day08")
@@ -121,20 +156,20 @@ fun main() {
         with (part1()) {
             "Part 1 -> $this".println()
 
-            val correctAnswer = 19241
+            val correctAnswer: Long = 19_241
             check(this == correctAnswer) {
                 lazyMessage("You broke Part 1!", correctAnswer, this)
             }
         }
 
-//        with (part2()) {
-//            "Part 2 -> $this".println()
-//
-//            val correctAnswer = 250825971
-//            check(this == correctAnswer) {
-//                lazyMessage("You broke Part 2!", correctAnswer, this)
-//            }
-//        }
+        with (part2()) {
+            "Part 2 -> $this".println()
+
+            val correctAnswer = 9_606_140_307_013
+            check(this == correctAnswer) {
+                lazyMessage("You broke Part 2!", correctAnswer, this)
+            }
+        }
     }
 
     "That's it!".println()
