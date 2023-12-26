cf731628a9914d67611e73be5d391aebb1f07564
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2023-12-25 21:10:51 -0500

    Started on Day10

--- src/Day10.kt
+++ src/Day10.kt
@@ -1,4 +1,3 @@
-// TODO Ctrl + Cmd + G - selects all occurrences for editing
 class Day10() : AoCSolution() {
     override val description = "Day 10: Pipe Maze"
 
@@ -9,6 +8,39 @@ class Day10() : AoCSolution() {
     companion object {
         fun using(input: List<String>) = Day10()
     }
+}
+
+enum class Direction {
+    NONE, NORTH, SOUTH, EAST, WEST;
+}
+
+enum class PipeType(val symbol: Char) {
+    NS('|') {
+        override fun connections() = listOf(Direction.NORTH, Direction.SOUTH)
+    },
+    EW('-') {
+        override fun connections() = listOf(Direction.EAST, Direction.WEST)
+    },
+    NE('L') {
+        override fun connections() = listOf(Direction.NORTH, Direction.EAST)
+    },
+    NW('J') {
+        override fun connections() = listOf(Direction.NORTH, Direction.WEST)
+    },
+    SW('7') {
+        override fun connections() = listOf(Direction.SOUTH, Direction.WEST)
+    },
+    SE('F') {
+        override fun connections() = listOf(Direction.SOUTH, Direction.EAST)
+    },
+    GROUND('.') {
+        override fun connections() = emptyList<Direction>()
+    },
+    START('S') {
+        override fun connections() = emptyList<Direction>()
+    };
+
+    abstract fun connections(): List<Direction>
 }
 
 fun main() {
